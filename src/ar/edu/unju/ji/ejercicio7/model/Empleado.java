889d637c25188cfd1ee665d56c61f5ec61481794
Andres Cruz <andres_013c@yahoo.com.ar>
2024-04-04 23:56:49 -0300

    ej7-clase Empleado, constantes, intanciacion de objetos

---
+++ src/ar/edu/unju/ji/ejercicio7/model/Empleado.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.ji.ejercicio7.model;
+
+public class Empleado {
+	private String nombre;
+	private int legajo;
+	private float salario;
+	
+	private final float basico = 210000.00f;
+	private final float aumento_por_merito = 20000.00f;
+	
+	public Empleado(String nombre, int legajo, float salario) {
+		this.nombre = nombre;
+		this.legajo = legajo;
+		if(this.basico <= salario) {
+			this.salario = salario;
+		}else {
+			this.salario = this.basico;
+		}
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("Nombre del empleado: "+this.nombre);
+		System.out.println("Legajo: "+this.legajo);
+		System.out.println("Salario: "+this.salario);
+	}
+	
+	public void darAumento() {
+		this.salario += aumento_por_merito;
+	}
+}
